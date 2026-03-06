a00411e0c9301c81139f94bce0b0f88d0b41d0ca
Frant-A <antonijfrolov1@gmail.com>
2026-03-06 10:02:40 +0300

    Add files via upload

---
+++ task_3.py
@@ -0,0 +1,31 @@
+# inventory = {
+#     "herb": 3,
+#     "water": 5,
+#     "mushroom": 2,
+#     "crystal": 0
+# }
+#
+# recipes = {
+#     "health_potion": {"herb": 2, "water": 1},
+#     "mana_potion": {"crystal": 1, "water": 2},
+#     "invisibility_potion": {"mushroom": 3, "water": 2, "herb": 1}
+# }
+inventory = dict()
+recipes = dict()
+
+
+def add_ingredient(inventory, ingredient, amount):
+
+
+
+
+while True:
+    
+    print("0. Завершить программу\n1. Добавить игредиенты\n2. Сварить")
+    inventory = input()
+    
+
+    if a = "0":
+        break
+
+
